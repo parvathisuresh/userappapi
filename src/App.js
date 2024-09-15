b4f459a37e4e68de133336d7162cf1f3dff0bf88
Parvathisuresh <parvathisuresh0220@gmail.com>
2024-09-15 22:17:21 +0530

    userappapi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import AddUsers from './components/AddUsers';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SearchUsers from './components/SearchUsers';
+import ViewUsers from './components/ViewUsers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AddUsers/>}/>
+        <Route path="/search" element={<SearchUsers/>}/>
+        <Route path="/view" element={<ViewUsers/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
